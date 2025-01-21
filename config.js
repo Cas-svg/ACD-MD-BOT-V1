d654f5a6cc712368ffa7000d88dd0828fbc48137
Cas-svg <edgarmakwasha0@gmail.com>
2025-01-21 13:49:06 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
+    return text === fault ? true : true;
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "vENWwCpD#2GdJ0oSkS2ieTw0rT0xRc357yghdbg3OLjhmRTyuitQ",  //මෙතනට ඔයාගෙ සෙශන් කෝඩ් එක දාන්න
